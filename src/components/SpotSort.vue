d67c70c3c875b889c5ed025d2abc66c24fee44b9
hhwang <wang@ispan.com.tw>
2024-02-01 23:14:02 +0800

    First Commit

---
+++ src/components/SpotSort.vue
@@ -0,0 +1,27 @@
+<template>
+    <select class="form-select" @change="changeHandler" v-model="theSort">
+        <option value="spotId">景點編號升冪</option>
+        <option value="-spotId">景點編號降冪</option>
+        <option value="spotTitle">景點名稱升冪</option>
+        <option value="-spotTitle">景點名稱降冪</option>
+    </select>
+</template>
+    
+<script setup>
+import { reactive, ref } from 'vue'
+const theSort = ref("spotId")
+const sortItem = reactive({
+    "type": "asc",
+    "by": "spotId"
+})
+const emit = defineEmits(["sortChange"])
+const changeHandler = () => {
+    sortItem.type = theSort.value.startsWith('-') ? "desc" : "asc";
+    sortItem.by = theSort.value.replace(/^-/, '');
+    console.log(sortItem);
+    emit("sortChange", sortItem)
+}
+
+</script>
+    
+<style></style>
